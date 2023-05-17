0aa9393c4e117ccff07005cc54f78425a51bc4a9
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2023-05-17 19:49:54 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import shutil
+from plugins.youtube import download
+from flask import Flask, render_template, request, send_file, Response
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/youtube')
+def yt():
+    return render_template("youtube.html")
+
+@app.route('/youtube/download', methods=["POST"])
+def ytdl():
+    video_url = request.form.get('url')
+    file_type = request.form.get('type')
+    response = download(video_url, file_type)
+    # Handle the downloaded stream as needed
+    if(response):
+      return send_file(response['file_path'], download_name=response['stream'].default_filename, as_attachment=True, mimetype='video/mp4')
+    return send_file(response['file_path'], download_name=response['stream'].default_filename, as_attachment=True, mimetype='audio/mp3')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,19 +1,22 @@
 from pytube import YouTube
+import os
+current_directory = os.getcwd()
+os.environ["IMAGEIO_FFMPEG_EXE"] = current_directory + "/ffmpeg/bin/ffmpeg"
+from moviepy.editor import *
 
-# Ask the user for the YouTube video URL
-video_url = input("Enter the YouTube video URL: ")
+print("YouTube plugin is up.")
+
+#https://youtu.be/DlIYupicuqM
 
 # Create a YouTube object with the video URL
-youtube = YouTube(video_url)
+def download(video_url,file_type):
+  youtube = YouTube(video_url)
+  # Get the highest resolution stream available
+  stream = youtube.streams.get_highest_resolution()
+  file_path = stream.download()
+  if(file_type == 'mp4'):
+    return { "file_path": file_path, "stream": stream }
+  video = VideoFileClip(file_path)
+  video.audio.write_audiofile("example.mp3")
 
-# Get the highest resolution stream available
-stream = youtube.streams.get_highest_resolution()
-
-# Get the default output filename
-output_filename = stream.default_filename
-
-# Download the video
-print(f"Downloading video: {output_filename}")
-stream.download()
-
-print("Video downloaded successfully!")
+  return False
